04144818f774fe1b903cfb7bdec36ab8c5c5883d
Max J. Polster <max.polster@id-p.de>
2019-01-11 23:26:11 +0100

    Hopefully fixed deploy script.

--- pipeline/deploy.js
+++ pipeline/deploy.js
@@ -3,23 +3,26 @@
 const path = require('path')
 const {publish} = require('gh-pages')
 
-module.exports = async ctx => {
-	await new Promise((resolve, reject) => {
-		publish(path.resolve(__dirname, '../dist'), {
-			repo: `https://${process.env.GH_TOKEN}@${process.env.DEPLOY_REPO}.git`,
-			branch: 'master',
-			silent: true,
-			message: process.env.DEPLOY_MESSAGE,
-			user: {
+module.exports = async ctx => new Promise((resolve, reject) => {
+	console.log(`Deploying to: ${process.env.DEPLOY_REPO}`)
+	publish(path.resolve(__dirname, '../dist'), {
+		repo: process.env.GH_TOKEN
+			? `https://${process.env.GH_TOKEN}@${process.env.DEPLOY_REPO}.git`
+			: `https://${process.env.DEPLOY_REPO}.git`,
+		branch: 'master',
+		silent: true,
+		message: process.env.DEPLOY_MESSAGE || 'Updates',
+		user: process.env.DEPLOY_AS_NAME && process.env.DEPLOY_AS_EMAIL
+			? {
 				name: process.env.DEPLOY_AS_NAME,
 				email: process.env.DEPLOY_AS_EMAIL
 			}
-		}, err => {
-			if (err) {
-				reject(err)
-			} else {
-				resolve()
-			}
-		})
+			: undefined
+	}, err => {
+		if (err) {
+			reject(err)
+		} else {
+			resolve()
+		}
 	})
-}
+})
