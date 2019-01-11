db5607b02b37643926fb0eb08d92af3d029dcde3
Max J. Polster <max.polster@id-p.de>
2019-01-11 22:22:35 +0100

    Deploy to github repo.

--- deploy-from-travis.js
+++ deploy-from-travis.js
@@ -1,3 +1,16 @@
 'use strict'
 
-console.log(process.env.TRAVIS_TEST)
+require('gh-pages').publish('dist', {
+	repo: `https://${process.env.GH_TOKEN}@github.com/phylumjs/phylumjs.github.io.git`,
+	silent: true,
+	branch: 'master',
+	user: {
+		name: 'Max J. Polster',
+		email: 'max.polster@id-p.de'
+	}
+}, err => {
+	if (err) {
+		console.error(err)
+		process.exit(1)
+	}
+})
