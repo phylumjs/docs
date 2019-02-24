c9300f95ba139f3c857e18f0b9c03c22654663b2
Max J. Polster <max.polster@id-p.de>
2019-02-24 11:55:51 +0100

    Remove travis config.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 'use strict'
 
-import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
-import components from './components'
-import router from './router'
+import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+import components from './components';
+import router from './router';
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 
 const app = new Vue(Object.assign({
 	router
-}, components.get('app-root')))
+}, components.get('app-root')));
 
-app.$mount('#app')
+app.$mount('#app');
--- src/router.js
+++ src/router.js
@@ -1,25 +1,12 @@
 'use strict'
 
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-import {home, sections} from './pages'
-
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
 	routes: [
-		home,
-		...(function * () {
-			for (const section of sections) {
-				for (const page of section.pages) {
-					yield {
-						path: section.path + page.path,
-						component: page.component
-					}
-				}
-			}
-		})(),
 		{path: '*', component: () => import('./pages/404')}
 	]
-})
+});
